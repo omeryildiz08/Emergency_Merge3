f956918c80a392d421e9b6917c67577bb0a28e6d
Omeryildiz08 <omer.yildiz27@hotmail.com>
2025-12-15 16:55:01 +0300

    data updates for gdd v0.4

--- Assets/Scripts/Grid/GridTileData.cs
+++ Assets/Scripts/Grid/GridTileData.cs
@@ -1,12 +1,17 @@
-
+[System.Serializable]
 public class GridTileData
 {
     
+    [System.NonSerialized]
     public GridTileView TileView;
 
-    
+    [System.NonSerialized]
     public MergeableObject ObjectOnTile;
 
     // Hızlı bir kontrol: Bu hücrede obje yoksa 'boş' demektir.
     public bool IsEmpty => ObjectOnTile == null;
+
+    //gdd v.04 
+    public bool isLocked = false;
+    public bool hasAnomaly = false;
 }
--- Assets/Scripts/SO/MergeableItemData.cs
+++ Assets/Scripts/SO/MergeableItemData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New MergeableItem", menuName = "Merge Game/Mergeable Item")]
 public class MergeableItemData : ScriptableObject
 {
-    
+    [Header ("Temel Bilgiler")]
     public string ItemName;
 
     
@@ -17,4 +17,11 @@ public class MergeableItemData : ScriptableObject
 
    
     public MergeableItemData NextLevelItem;
+
+    [Header("ekonomi ve ilerleme(GDD V.0.4)")]
+    public int SellPrice; //Gdd:Zaman kredisi değeri (satış)
+    public int BuyPrice; //marketten alış
+    
+    public int FacilityPoint; //üs genişletme puanı 
+
 }
